4ccd9efae7282d9599b414f5007903bb17d65b10
asialit <asia_litwin@hotmail.com>
2021-04-28 19:49:47 +0200

    Add Candidate model

--- app/models.py
+++ app/models.py
@@ -1,18 +1,8 @@
-from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
-
-from app import db, login
+from app import db
 
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
-
-
-class User(UserMixin, db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
-    surname = db.Column(db.String(64))
     username = db.Column(db.String(64), unique=True)
     password_hash = db.Column(db.String(128))
     is_admin = db.Column(db.Boolean)
@@ -20,8 +10,8 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return f"User {self.username}"
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+class Candidate(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    surname = db.Column(db.String(64))
